3b2da562560aeef780122b7c134ddeb80b6e3224
hajos8 <rabb.andras@gmail.com>
2026-01-22 09:58:21 +0100

    Add .gitignore for local Netlify folder and update fruits function to use Response object

--- react-app/netlify/functions/fruits.js
+++ react-app/netlify/functions/fruits.js
@@ -1,8 +1,8 @@
-export default function (request, context) {
+export default async function (request, context) {
     console.log("Fruits function invoked");
     console.log("Request method:", request.method);
     console.log("Request headers:", request.headers);
     console.log("Request body:", request.body);
 
-    return { statusCode: 200, body: JSON.stringify(['Apple', 'Banana', 'Cherry']) };
+    return new Response(JSON.stringify(['Apple', 'Banana', 'Cherry']), { status: 200 });
 }
